f242d24ec551a22df5d8aa0a15ae6817910cbd33
Umesh Kumar Swain <umeshkumarswain5@gmail.com>
2022-07-17 04:50:52 +0530

    email validation completed

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -4,6 +4,8 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import IUser from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth.service';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +14,7 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class RegisterComponent {
   name = new FormControl('', [Validators.required, Validators.minLength(3)])
-  email = new FormControl('', [Validators.required, Validators.email])
+  email = new FormControl('', [Validators.required, Validators.email], [this.emailTaken.validate])
   age = new FormControl<number | null>(null, [Validators.required, Validators.min(18), Validators.max(120)])
   password = new FormControl('', [Validators.required, Validators.min(8), Validators.max(20)])
   confirm_password = new FormControl('', [Validators.required,])
@@ -25,11 +27,11 @@ export class RegisterComponent {
   inSubmission: boolean = false;
 
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, private emailTaken: EmailTaken) { }
 
   registerForm = new FormGroup({
     name: this.name, email: this.email, age: this.age, password: this.password, confirm_password: this.confirm_password, phoneNumber: this.phoneNumber
-  });
+  }, [RegisterValidators.match("password", "confirm_password")]);
 
   async register() {
     this.showAlert = true;
